b7e61a1936812b7f56bb5da59a111ebf5676aa79
Veronika Strelcova <vera20.4@yandex.by>
2023-04-29 20:49:19 +0300

    Revert "Revert "Merge pull request #2 from HidJaba-hub/revert-1-lecture-1-setup-and-product-dao""
    This reverts commit 5bca12392a5c13e1a7b07ad16c2851de0b02d52b.

--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -1,19 +1,12 @@
 package com.es.phoneshop.model.product;
 
-import com.es.phoneshop.model.product.DAO.ArrayListProductDao;
-import com.es.phoneshop.model.product.DAO.ProductDao;
-import com.es.phoneshop.model.product.entity.Product;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.math.BigDecimal;
-import java.util.Currency;
-import java.util.List;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
-
-public class ArrayListProductDaoTest {
-    Currency usd = Currency.getInstance("USD");
+public class ArrayListProductDaoTest
+{
     private ProductDao productDao;
 
     @Before
@@ -23,35 +16,6 @@ public class ArrayListProductDaoTest {
 
     @Test
     public void testFindProductsNoResults() {
-        Product product = productDao.save(new Product("test", "test", new BigDecimal(100), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
-        assertTrue(product.getId() > 0);
-        List<Product> products = productDao.findProducts();
-        assertFalse(products.isEmpty());
-        assertFalse(products.stream()
-                .anyMatch(product1 -> product1.equals(product)));
-    }
-
-    @Test
-    public void testSaveProduct() {
-        Product product = productDao.save(new Product("test", "test", new BigDecimal(100), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
-
-        assertTrue(product.getId() > 0);
-        productDao.save(product);//add the same product to check repeatability
-        assertNotNull(productDao.getProduct(product.getId()));
-        assertEquals("test", product.getCode());
-    }
-
-    @Test
-    public void testDeleteProduct() {
-        Product product = productDao.save(new Product("test", "test", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
-        assertTrue(product.getId() > 0);
-
-        List<Product> products = productDao.findProducts();
-        assertFalse(products.isEmpty());
-        assertTrue(products.stream()
-                .anyMatch(product1 -> product1.equals(product)));
-        productDao.delete(product.getId());
-        assertFalse(productDao.findProducts().stream()
-                .anyMatch(product1 -> product1.equals(product)));
+        assertTrue(productDao.findProducts().isEmpty());
     }
 }
