df6e134e6eb13b28e4ca52616fab056c478b5021
esSoftPalinaTsi <132053406+esSoftPalinaTsi@users.noreply.github.com>
2023-04-29 19:30:54 +0300

    Revert "Task 1.5 Implemented ProductDao, task 1.6: Use ProductDao on PLP"

--- src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
+++ src/test/java/com/es/phoneshop/web/ProductListPageServletTest.java
@@ -1,36 +1,34 @@
 package com.es.phoneshop.web;
 
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductListPageServletTest {
-    private final ProductListPageServlet servlet = new ProductListPageServlet();
     @Mock
     private HttpServletRequest request;
     @Mock
     private HttpServletResponse response;
     @Mock
     private RequestDispatcher requestDispatcher;
-    @Mock
-    private ServletConfig config;
+
+    private ProductListPageServlet servlet = new ProductListPageServlet();
 
     @Before
-    public void setup() throws ServletException {
-        servlet.init(config);
+    public void setup(){
         when(request.getRequestDispatcher(anyString())).thenReturn(requestDispatcher);
     }
 
@@ -39,32 +37,5 @@ public class ProductListPageServletTest {
         servlet.doGet(request, response);
 
         verify(requestDispatcher).forward(request, response);
-        verify(request).setAttribute(eq("products"), any());
-    }
-    @Test
-    public void testDoPostGetProduct() throws ServletException, IOException {
-        when(request.getParameter("action")).thenReturn("findProduct");
-        when(request.getParameter("phoneId")).thenReturn("-1");
-        servlet.doPost(request, response);
-
-        verify(requestDispatcher).forward(request, response);
-        verify(request).setAttribute(eq("products"), any());
-    }
-    @Test
-    public void testDoPostDelete() throws ServletException, IOException {
-        when(request.getParameter("action")).thenReturn("deleteProducts");
-        when(request.getParameter("phoneIdToDelete")).thenReturn("-1");
-        servlet.doPost(request, response);
-
-        verify(requestDispatcher).forward(request, response);
-        verify(request).setAttribute(eq("products"), any());
-    }
-    @Test
-    public void testDoPostFindNotNullProducts() throws ServletException, IOException {
-        when(request.getParameter("action")).thenReturn("findNotNullProducts");
-        servlet.doPost(request, response);
-
-        verify(requestDispatcher).forward(request, response);
-        verify(request).setAttribute(eq("products"), any());
     }
 }
